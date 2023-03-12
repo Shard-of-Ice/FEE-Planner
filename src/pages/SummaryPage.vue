2f998dcf879d366ee6b889597f284bbd9401d436
Ice Shard <ice.shard@sfr.fr>
2023-03-12 17:36:24 +0100

    [UI] Added level selection

--- src/pages/SummaryPage.vue
+++ src/pages/SummaryPage.vue
@@ -13,15 +13,27 @@
       />
       <p>{{ unit.class.type.name }}</p>
       <div class="row justify-between full-width">
-        <p class="col-2">Level</p>
-        <p class="col-1 text-right">{{ unit.level }}</p>
-        <p class="col-1 text-left">({{ unit.totalLevel }})</p>
+        <q-select
+          outlined
+          class="col-6"
+          v-model="unit.level"
+          :options="allowedLevels"
+          :option-label="
+            (l) =>
+              '' +
+              l +
+              ' (' +
+              ((unit?.totalLevel || 1) - (unit?.level || 1) + l) +
+              ')'
+          "
+          label="Level"
+        />
         <p class="col-2">Mov</p>
         <p class="col-1 text-right">{{ unit.stats.mov }}</p>
       </div>
       <p>HP {{ unit.stats.hp }} / {{ unit.stats.hp }}</p>
       <p>Bld {{ unit.stats.bld }}</p>
-      <p>SP {{ unit.stats.sp }}</p>
+      <p>SP {{ unit.sp }}</p>
       <h3>Combat Stats</h3>
       <template v-for="(statValue, statName) in combatStats" :key="statName">
         <div class="row justify-between full-width">
@@ -41,29 +53,32 @@
   >
 </template>
 
-<script>
-import { computed } from 'vue';
+<script lang="ts">
+import { computed, ComputedRef } from 'vue';
 import { useAppStateStore } from 'src/stores/AppStateStore';
 import { useStaticStore } from 'src/stores/StaticStore';
+import { Unit } from 'src/stores/datatypes';
 
 export default {
   setup() {
     const appStateStore = useAppStateStore();
     const staticStore = useStaticStore();
 
-    const unit = computed(() => appStateStore.selectedUnit);
+    const unit: ComputedRef<Unit | null> = computed(
+      () => appStateStore.selectedUnit
+    );
 
     return {
       unit,
       basicStats: computed(() => {
         return {
-          Str: appStateStore.selectedUnit.stats.str,
-          Mag: appStateStore.selectedUnit.stats.mag,
-          Dex: appStateStore.selectedUnit.stats.dex,
-          Spd: appStateStore.selectedUnit.stats.spd,
-          Def: appStateStore.selectedUnit.stats.def,
-          Res: appStateStore.selectedUnit.stats.res,
-          Lck: appStateStore.selectedUnit.stats.lck,
+          Str: unit.value?.stats.str || 0,
+          Mag: unit.value?.stats.mag || 0,
+          Dex: unit.value?.stats.dex || 0,
+          Spd: unit.value?.stats.spd || 0,
+          Def: unit.value?.stats.def || 0,
+          Res: unit.value?.stats.res || 0,
+          Lck: unit.value?.stats.lck || 0,
         };
       }),
       combatStats: computed(() => {
@@ -80,6 +95,17 @@ export default {
           (clss) => unit.value && unit.value.character.canUseClass(clss)
         )
       ),
+      allowedLevels: computed(() => {
+        let min = 1;
+        let max = unit.value?.class.tier.maxLevel || 1;
+        if (
+          unit.value &&
+          unit.value.class == unit.value.character.startingClass
+        ) {
+          min = unit.value?.character.startingLevel;
+        }
+        return Array.from({ length: max - min + 1 }, (_, i) => min + i);
+      }),
     };
   },
 };
