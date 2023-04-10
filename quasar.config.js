dd1b69d49050d157635ae328508aab0b67862b45
Ice Shard <ice.shard@sfr.fr>
2023-04-10 20:46:41 +0200

    [BUILD] Removed gh-pages publicpath as the site now has a dedicated domain name

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: process.env.NODE_ENV === 'development' ? '/' : '/FEE-Planner',
+      // publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
