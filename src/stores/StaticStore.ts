4ec93715678b5d3c85cfa8d5877026c0fcd3d3c6
Ice Shard <ice.shard@sfr.fr>
2023-06-10 23:11:24 +0200

    [REFACTORING] Extracted CSV parsing from pinia store

--- src/stores/StaticStore.ts
+++ src/stores/StaticStore.ts
@@ -1,7 +1,11 @@
 import { defineStore } from 'pinia';
 import { Character } from 'src/models/Character';
-import { Class, ClassTier, ClassType } from 'src/models/Class';
-import { StatBlock } from 'src/models/StatBlock';
+import { Class } from 'src/models/Class';
+import {
+  readAllCharacters,
+  readAllClasses,
+  readCsvFromUrl,
+} from 'src/utils/CsvParsing';
 
 interface StaticStoreState {
   classes: { [key: string]: Class };
@@ -9,129 +13,9 @@ interface StaticStoreState {
   playableCharacters: string[];
 }
 
-function getClassByName(
-  className: string,
-  state: StaticStoreState,
-  characterName: string
-) {
-  for (const classId in state.classes) {
-    const clss = state.classes[classId];
-    // We filter out classes with the same name, but for the wrong character
-    if (
-      clss.name == className &&
-      (clss.exclusiveCharacterName == '' ||
-        clss.exclusiveCharacterName == characterName)
-    ) {
-      return clss;
-    }
-  }
-  // default
-  return state.classes[Object.keys(state.classes)[0]];
-}
-
-type StringDict = { [key: string]: string };
-type StringDictDict = { [id: string]: StringDict };
-
-function readCsv(url: string): Promise<StringDictDict> {
-  const result: StringDictDict = {};
-
-  return new Promise<StringDictDict>(function (resolve) {
-    // some async operation here
-    fetch(url, {
-      method: 'get',
-      headers: {
-        'content-type': 'text/csv;charset=UTF-8',
-        //'Authorization': //in case you need authorisation
-      },
-    })
-      .then((data) => data.text())
-      .then((text) => {
-        const lines = text.split('\n');
-        const header = lines[0].split(';');
-        lines.slice(1).forEach((line) => {
-          if (!(line.startsWith('Name') || line.length < 20)) {
-            const values = line.split(';');
-            if (values.length == header.length) {
-              const lineDict: StringDict = {};
-              for (let i = 0; i < header.length; ++i) {
-                lineDict[header[i]] = values[i];
-              }
-              result[lineDict['ID']] = lineDict;
-            }
-          }
-        });
-        // resolve the promise with some value
-        resolve(result);
-      });
-  });
-}
-
-function statBlockFromDict(
-  data: StringDict,
-  prefix = '',
-  suffix = ''
-): StatBlock {
-  return new StatBlock(
-    Number(data[prefix + 'HP' + suffix] ?? '0'),
-    Number(data[prefix + 'Str' + suffix] ?? '0'),
-    Number(data[prefix + 'Mag' + suffix] ?? '0'),
-    Number(data[prefix + 'Dex' + suffix] ?? '0'),
-    Number(data[prefix + 'Spd' + suffix] ?? '0'),
-    Number(data[prefix + 'Def' + suffix] ?? '0'),
-    Number(data[prefix + 'Res' + suffix] ?? '0'),
-    Number(data[prefix + 'Lck' + suffix] ?? '0'),
-    Number(data[prefix + 'Bld' + suffix] ?? '0'),
-    Number(data[prefix + 'Mov' + suffix] ?? '0')
-  );
-}
-
-function classFromDict(data: StringDict): Class {
-  return new Class(
-    data['ID'],
-    data['Name'],
-    ClassTier.fromString(data['Class Tier']),
-    ClassType.fromString(data['Class Type']),
-    statBlockFromDict(data, 'Base '),
-    statBlockFromDict(data, '', ' Growth'),
-    statBlockFromDict(data, '', ' Cap'),
-    data['Flag'].length > 4,
-    data['Flag'] == 'Female-only',
-    data['Exclusive User']
-  );
-}
-
-function readAllClasses(data: StringDictDict, state: StaticStoreState) {
-  for (const key in data) {
-    state.classes[key] = classFromDict(data[key]);
-  }
-}
-
-function characterFromDict(
-  data: StringDict,
-  state: StaticStoreState
-): Character {
-  return new Character(
-    data['ID'],
-    data['Name'],
-    getClassByName(data['Initial Class'], state, data['Name']),
-    Number(data['Level']),
-    Number(data['Internal Level']),
-    Number(data['SP']),
-    statBlockFromDict(data, '', ' Base'),
-    statBlockFromDict(data, '', ' Growth'),
-    statBlockFromDict(data, '', ' Cap+'),
-    data['Gender'] == 'Female'
-  );
-}
-
-function readAllCharacters(data: StringDictDict, state: StaticStoreState) {
-  for (const key in data) {
-    state.characters[key] = characterFromDict(data[key], state);
-    if (state.characters[key].startingSP > 0 && !key.endsWith('竜化')) {
-      // The second check is for Nel and Rafal transformed, otherwise they appear 2 times
-      state.playableCharacters.push(key);
-    }
-  }
+function isPlayable(key: string, character: Character) {
+  // The second check is for Nel and Rafal transformed, otherwise they appear 2 times
+  return character.startingSP > 0 && !key.endsWith('竜化');
 }
 
 export const useStaticStore = defineStore('static', {
@@ -153,11 +37,14 @@ export const useStaticStore = defineStore('static', {
   actions: {
     async loadStaticStore() {
       await Promise.all([
-        readCsv('data/classes.csv'),
-        readCsv('data/characters.csv'),
+        readCsvFromUrl('data/classes.csv'),
+        readCsvFromUrl('data/characters.csv'),
       ]).then((values) => {
-        readAllClasses(values[0], this);
-        readAllCharacters(values[1], this);
+        this.classes = readAllClasses(values[0]);
+        this.characters = readAllCharacters(values[1], this.classes);
+        this.playableCharacters = Object.keys(this.characters).filter((key) =>
+          isPlayable(key, this.characters[key])
+        );
       });
     },
   },
---
+++ src/utils/CsvParsing.ts
@@ -0,0 +1,133 @@
+import { Character } from 'src/models/Character';
+import { Class, ClassTier, ClassType } from 'src/models/Class';
+import { StatBlock } from 'src/models/StatBlock';
+
+export type StringDict = { [key: string]: string };
+export type StringDictDict = { [id: string]: StringDict };
+
+export type ClassDict = { [key: string]: Class };
+export type CharacterDict = { [key: string]: Character };
+
+export function readCsvFromUrl(url: string): Promise<StringDictDict> {
+  return new Promise<StringDictDict>(function (resolve) {
+    fetch(url, {
+      method: 'get',
+      headers: {
+        'content-type': 'text/csv;charset=UTF-8',
+      },
+    })
+      .then((data) => data.text())
+      .then((text) => {
+        resolve(readCsv(text));
+      });
+  });
+}
+
+export function readCsv(text: string): StringDictDict {
+  const result: StringDictDict = {};
+
+  const lines = text.split('\n');
+  const header = lines[0].split(';');
+  lines.slice(1).forEach((line) => {
+    if (!(line.startsWith('Name') || line.length < 20)) {
+      const values = line.split(';');
+      if (values.length == header.length) {
+        const lineDict: StringDict = {};
+        for (let i = 0; i < header.length; ++i) {
+          lineDict[header[i]] = values[i];
+        }
+        result[lineDict['ID']] = lineDict;
+      }
+    }
+  });
+
+  return result;
+}
+
+function statBlockFromDict(
+  data: StringDict,
+  prefix = '',
+  suffix = ''
+): StatBlock {
+  return new StatBlock(
+    Number(data[prefix + 'HP' + suffix] ?? '0'),
+    Number(data[prefix + 'Str' + suffix] ?? '0'),
+    Number(data[prefix + 'Mag' + suffix] ?? '0'),
+    Number(data[prefix + 'Dex' + suffix] ?? '0'),
+    Number(data[prefix + 'Spd' + suffix] ?? '0'),
+    Number(data[prefix + 'Def' + suffix] ?? '0'),
+    Number(data[prefix + 'Res' + suffix] ?? '0'),
+    Number(data[prefix + 'Lck' + suffix] ?? '0'),
+    Number(data[prefix + 'Bld' + suffix] ?? '0'),
+    Number(data[prefix + 'Mov' + suffix] ?? '0')
+  );
+}
+
+function classFromDict(data: StringDict): Class {
+  return new Class(
+    data['ID'],
+    data['Name'],
+    ClassTier.fromString(data['Class Tier']),
+    ClassType.fromString(data['Class Type']),
+    statBlockFromDict(data, 'Base '),
+    statBlockFromDict(data, '', ' Growth'),
+    statBlockFromDict(data, '', ' Cap'),
+    data['Flag'].length > 4,
+    data['Flag'] == 'Female-only',
+    data['Exclusive User']
+  );
+}
+
+export function readAllClasses(data: StringDictDict): ClassDict {
+  const classes: ClassDict = {};
+  for (const key in data) {
+    classes[key] = classFromDict(data[key]);
+  }
+  return classes;
+}
+
+function getClassByName(
+  className: string,
+  classes: ClassDict,
+  characterName: string
+) {
+  for (const classId in classes) {
+    const clss = classes[classId];
+    // We filter out classes with the same name, but for the wrong character
+    if (
+      clss.name == className &&
+      (clss.exclusiveCharacterName == '' ||
+        clss.exclusiveCharacterName == characterName)
+    ) {
+      return clss;
+    }
+  }
+  // default
+  return classes[Object.keys(classes)[0]];
+}
+
+function characterFromDict(data: StringDict, classes: ClassDict): Character {
+  return new Character(
+    data['ID'],
+    data['Name'],
+    getClassByName(data['Initial Class'], classes, data['Name']),
+    Number(data['Level']),
+    Number(data['Internal Level']),
+    Number(data['SP']),
+    statBlockFromDict(data, '', ' Base'),
+    statBlockFromDict(data, '', ' Growth'),
+    statBlockFromDict(data, '', ' Cap+'),
+    data['Gender'] == 'Female'
+  );
+}
+
+export function readAllCharacters(
+  data: StringDictDict,
+  classes: ClassDict
+): CharacterDict {
+  const characters: CharacterDict = {};
+  for (const key in data) {
+    characters[key] = characterFromDict(data[key], classes);
+  }
+  return characters;
+}
